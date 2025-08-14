8254ee701924cb5c32309adc7ee69325bc13ece7
JosueVJ <josuevj.1e.49@gmail.com>
2025-08-14 12:30:32 -0600

    update portafolio

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -1,1 +1,8 @@
 export const URL = 'https://unicorn-sparkle.web.app'
+
+// CV Links - Replace these with your actual Google Drive sharing links
+// To get the link: 1. Upload your CV to Google Drive 2. Right click -> Get link 3. Make sure it's set to "Anyone with the link can view"
+export const CV_LINKS = {
+  en: 'https://drive.google.com/file/d/1LrnEmxwQVL2at3yVcetav3ySKVkmiDby/view?usp=drive_link', // Replace with your English CV Google Drive link
+  es: 'https://drive.google.com/file/d/1OQr4ZbePUEL5Gb9vaFAvI-gYypX8mYn1/view?usp=drive_link'  // Replace with your Spanish CV Google Drive link
+}
